9c589d18bf7815d25ba5e491cd7562de37887d4b
Walter Fritsche <wfritsche95@protonmail.com>
2024-11-11 09:58:02 -0600

    added react query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,16 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Navbar from "../components/Navbar";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
+      <Navbar />
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
+      <ReactQueryDevtools position="bottom-right" />
     </>
   ),
 });
