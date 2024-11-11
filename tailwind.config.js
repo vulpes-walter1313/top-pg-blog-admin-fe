1b61e77c32f5d8b5a98e5426369d76d9294a7724
Walter Fritsche <wfritsche95@protonmail.com>
2024-11-11 09:55:41 -0600

    added roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,36 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: [
+        "Roboto",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      serif: [
+        "ui-serif",
+        "Georgia",
+        "Cambria",
+        "Times New Roman",
+        "Times",
+        "serif",
+      ],
+      mono: [
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+        "monospace",
+      ],
+    },
     extend: {
       fontSize: {
         deskh1: ["3.375rem", { lineHeight: "115%", fontWeight: "700" }],
